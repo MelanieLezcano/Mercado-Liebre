6b30c8d9f52fe633afbb2aa8d16c16812dc697fb
melanieLezcano <melanie.s.lezcano@gmail.com>
2022-06-27 14:48:31 -0300

    se agrega logo y lista a header

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
 app.get('/',(req,res) => {
-    res.sendFile(path.join(__dirname, './views/'));
+    res.sendFile(path.join(__dirname, './views/home.html'));
 });
 
 app.listen(3000, () => console.log("servidor levantado")
