17f5bcc8a8f6881c5a5efe9afb834a250f321ca5
MelanieLezcano <melanie.s.lezcano@gmail.com>
2022-07-28 16:32:25 -0300

    arreglo estructura html, agrego css: footer, general, header, main, styles y variables

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const publicPath = path.resolve(__dirname, './public');
+let port = 3000;
 
 app.use(express.static(publicPath));
 
@@ -9,5 +10,5 @@ app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));
 });
 
-app.listen(3000, () => console.log("servidor levantado")
+app.listen(port, () => console.log("servidor levantado")
 );
